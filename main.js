ae7f949e12212b48837e0203a3169a2c144a7282
Carlos M Garcia <cgarcia101015@gmail.com>
2018-11-11 23:57:01 -0500

    YT api

---
+++ main.js
@@ -0,0 +1,67 @@
+// Options
+const CLIENT_ID = '374392875072-qcs4v036q0ndjeeg39v6f87pf3c5qa0g.apps.googleusercontent.com';
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
+const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+
+const authorizeButton = document.getElementById('authorize-button');
+const signoutButton = document.getElementById('signout-button');
+
+const content = document.getElementById('content');
+const chanelForm = document.getElementById('chanel-form');
+const channelInput = document.getElementById('chanel-input');
+const videoContainer = document.getElementById('video-container');
+
+const defaultChannel = 'TheStraightPipes';
+
+// Load auth2 Library
+function handleClientLoad() {
+    gapi.load('client:auth2', iniClient);
+}
+
+// Init API client library and set up sign in listeners
+function iniClient() {
+    gapi.client.init({
+        dicoveryDocs: DISCOVERY_DOCS,
+        clientId: CLIENT_ID,
+        scope: SCOPES
+    }).then(() => {
+        // Listen for sign in state changes
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+        // Handle initial sign in state
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    });
+}
+
+function updateSigninStatus(isSignedIn) {
+    if(isSignedIn) {
+      authorizeButton.style.display = 'none';
+      signoutButton.style.display = 'block';
+      content.style.display = 'block';
+      videoContainer.style.display = 'block';
+      getChannel(defaultChannel);
+     } else {
+      authorizeButton.style.display = 'block';
+      signoutButton.style.display = 'none';
+      content.style.display = 'none';
+      videoContainer.style.display = 'none';
+
+    }
+}
+
+// Handle login
+function handleAuthClick() {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+// Handle logout
+function handleSignoutClick() {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+// Get channel from API
+function getChannel (channel) {
+  console.loc(channel);  
+}
+
